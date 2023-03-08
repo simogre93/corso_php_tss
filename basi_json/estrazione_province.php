193b194c29164092e4e13bd80eff1aeb630e8a8c
Simone <simogre93@libero.it>
2023-03-08 11:35:30 +0100

    fine estrazione province

--- basi_json/estrazione_province.php
+++ basi_json/estrazione_province.php
@@ -6,15 +6,6 @@ include "config.php";
 $province_string = file_get_contents('province.json');
 $province_object = json_decode($province_string);
 
-//var_dump($province_object);
-
-//trasfoma array in un altro array 
-$province_array = array_map(function($provincia){
-     return $provincia->nome;
-     return $provincia->sigla;
-     return $provincia->regione;
- }, $province_object);
-
 $dsn = "mysql:host=".DB_HOST.";dbname=".DB_NAME;
 
 try {
@@ -26,7 +17,8 @@ try {
         
         $provincia_nome = addslashes($provincia->nome);
         $provincia_sigla = addslashes($provincia->sigla);
-        $regione_id =  $conn->query("SELECT regione_id FROM regioni WHERE nome=\"$provincia->regione\"");
+        $regione_id = $provincia->regione;
+        $regione_id =  $conn->query("SELECT regione_id FROM regioni WHERE nome=\"$provincia->regione\"")->fetchColumn();
         
         $sql = "INSERT INTO province (nome, sigla, regione_id) VALUES ('$provincia_nome' , '$provincia_sigla' , '$regione_id');";
         echo $sql ."\n";
