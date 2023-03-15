6e7b069c8fd8866e21dc17926f27a3491aa5a9b7
Simone <simogre93@libero.it>
2023-03-15 12:59:14 +0100

    classe CRUD

---
+++ form_in_php/class/crud/UserCRUD.php
@@ -0,0 +1,25 @@
+<?php
+namespace crud;
+
+class UserCRUD {
+
+    public function create()
+    {
+        
+    }
+
+    public function update()
+    {
+        
+    }
+
+    public function read()
+    {
+        
+    }
+
+    public function delete()
+    {
+        
+    }
+}
